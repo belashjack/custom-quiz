470891adbd9e3e2fa1ccf817cc047444a2115f7b
Yauheni Belash <yauheni.belash@craft.co>
2024-10-14 19:07:29 +0200

    Rename links to point to CloudFront

--- src/rounds/constants.tsx
+++ src/rounds/constants.tsx
@@ -4,152 +4,152 @@ import { Explanation } from './types';
 export const CORRECT_EXPLANATION_ASSETS: Explanation['asset'][] = [
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/correctAnswers/video1.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/correctAnswers/video1.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/correctAnswers/video2.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/correctAnswers/video2.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/correctAnswers/video3.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/correctAnswers/video3.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/correctAnswers/video4.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/correctAnswers/video4.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/correctAnswers/video5.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/correctAnswers/video5.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/correctAnswers/video6.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/correctAnswers/video6.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/correctAnswers/video7.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/correctAnswers/video7.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/correctAnswers/video8.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/correctAnswers/video8.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/correctAnswers/video9.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/correctAnswers/video9.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/correctAnswers/video10.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/correctAnswers/video10.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/correctAnswers/video11.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/correctAnswers/video11.webm"
             type="video/webm"
         />
     </video>,
-    <img src="https://custom-quiz-assets.s3.amazonaws.com/config1/correctAnswers/image1.webp" />,
-    <img src="https://custom-quiz-assets.s3.amazonaws.com/config1/correctAnswers/image2.webp" />,
-    <img src="https://custom-quiz-assets.s3.amazonaws.com/config1/correctAnswers/image3.webp" />,
+    <img src="https://assets.happybirthdaydianayasenko.com/config1/correctAnswers/image1.webp" />,
+    <img src="https://assets.happybirthdaydianayasenko.com/config1/correctAnswers/image2.webp" />,
+    <img src="https://assets.happybirthdaydianayasenko.com/config1/correctAnswers/image3.webp" />,
 ];
 
 export const INCORRECT_EXPLANATION_ASSETS: Explanation['asset'][] = [
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/video1.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/video1.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/video2.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/video2.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/video3.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/video3.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/video4.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/video4.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/video5.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/video5.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/video6.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/video6.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/video7.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/video7.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/video8.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/video8.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/video9.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/video9.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/video10.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/video10.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/video11.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/video11.webm"
             type="video/webm"
         />
     </video>,
     <video autoPlay loop muted playsInline>
         <source
-            src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/video12.webm"
+            src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/video12.webm"
             type="video/webm"
         />
     </video>,
-    <img src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/image1.webp" />,
-    <img src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/image1.webp" />,
-    <img src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/image1.webp" />,
-    <img src="https://custom-quiz-assets.s3.amazonaws.com/config1/incorrectAnswers/image1.webp" />,
+    <img src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/image1.webp" />,
+    <img src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/image1.webp" />,
+    <img src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/image1.webp" />,
+    <img src="https://assets.happybirthdaydianayasenko.com/config1/incorrectAnswers/image1.webp" />,
 ];
 
 export const DEFAULT_TIMER_DURATION = 15000;
